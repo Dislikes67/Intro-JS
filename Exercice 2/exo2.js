cd7c4820ee3bf617bfa8b363b2cd50c3260f63bc
Mathias Rudolf <matt67creativ@hotmail.fr>
2024-07-05 10:39:47 +0200

    exo2 fin

--- Exercice 2/exo2.js
+++ Exercice 2/exo2.js
@@ -1,16 +1,28 @@
 const square = document.querySelectorAll(".square")
-console.log(square)
 const result = document.querySelector(".result")
 
-// console.log(block)
 square.forEach((square) => {
   square.addEventListener("click", () => {
-
     const bgcolor = getComputedStyle(square).backgroundColor
     result.style.backgroundColor = bgcolor;
     result.innerHTML = bgcolor;
   })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
