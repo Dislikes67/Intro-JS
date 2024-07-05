2d24365a10a0981e25fe0122aabda13ae0092e36
Mathias Rudolf <matt67creativ@hotmail.fr>
2024-07-05 10:55:38 +0200

    minor changes + commentaires exo2

--- Exercice 2/exo2.js
+++ Exercice 2/exo2.js
@@ -1,29 +1,10 @@
-const square = document.querySelectorAll(".square")
-const result = document.querySelector(".result")
+const square = document.querySelectorAll(".square")  // Sélectionne tout les .square et les stocke dans la variable squares.
+const result = document.querySelector(".result") // Sélectionne .result et le stocke dans la variable result.
 
-square.forEach((square) => {
+square.forEach((square) => {   // Pour chaque square ajoute un ecouteur d'evenement click à chaque square
   square.addEventListener("click", () => {
-    const bgcolor = getComputedStyle(square).backgroundColor
-    result.style.backgroundColor = bgcolor;
-    result.innerHTML = bgcolor;
+    const bgcolor = getComputedStyle(square).backgroundColor // Obtient la couleur de fond du carré cliqué.
+    result.style.backgroundColor = bgcolor; // Applique cette couleur de fond au conteneur de résultat.
+    result.innerHTML = bgcolor; // Affiche la couleur de fond en texte dans le conteneur de résultat.
   })
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//recup ma property color pour la mettre aux autres
