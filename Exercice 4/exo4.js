4dd74484181d68e08d98aab766f8edbf5918c2cd
Mathias Rudolf <matt67creativ@hotmail.fr>
2024-07-08 16:37:55 +0200

    save before major changes

--- Exercice 4/exo4.js
+++ Exercice 4/exo4.js
@@ -2,12 +2,22 @@
 const square = document.querySelectorAll(".square")
 // Select the container element
 const result = document.querySelector(".container")
+// Store the default background color of the container
+const defaultBgColor = getComputedStyle(result).backgroundColor;
 
 // Add a click event listener to each square
 square.forEach((square) => {
     square.addEventListener("click", ()  => {
         // Get the background color of the clicked square
         const bgColor = getComputedStyle(square).backgroundColor
+
+        // Toggle the background color of the container
+        if (result.style.backgroundColor === bgColor) {
+            result.style.backgroundColor = defaultBgColor;
+        } else {
+            result.style.backgroundColor = bgColor;
+        }
+        
         // Set the background color of the container to the color of the clicked square
         result.style.backgroundColor = bgColor;
         // Get the child [array] of the square
