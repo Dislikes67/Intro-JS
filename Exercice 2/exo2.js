96e56a051dff797c465d29e09b486564b4aa6e11
Mathias Rudolf <matt67creativ@hotmail.fr>
2024-07-08 14:23:58 +0200

    update JS

--- Exercice 2/exo2.js
+++ Exercice 2/exo2.js
@@ -3,8 +3,8 @@ const result = document.querySelector(".result") // Sélectionne .result et le s
 
 square.forEach((square) => {   // Pour chaque square ajoute un ecouteur d'evenement click à chaque square
   square.addEventListener("click", () => {
-    const bgcolor = getComputedStyle(square).backgroundColor // Obtient la couleur de fond du carré cliqué.
-    result.style.backgroundColor = bgcolor; // Applique cette couleur de fond au conteneur de résultat.
-    result.innerHTML = bgcolor; // Affiche la couleur de fond en texte dans le conteneur de résultat.
+    const bgColor = getComputedStyle(square).backgroundColor // Obtient la couleur de fond du carré cliqué.
+    result.style.backgroundColor = bgColor; // Applique cette couleur de fond au conteneur de résultat.
+    result.innerHTML = bgColor; // Affiche la couleur de fond en texte dans le conteneur de résultat.
   })
 })
