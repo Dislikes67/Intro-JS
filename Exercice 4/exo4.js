84a34b1c94c8a2c491a6b8b8945119771c2bbb2a
Mathias Rudolf <matt67creativ@hotmail.fr>
2024-07-08 16:43:20 +0200

    update JS active toggle

--- Exercice 4/exo4.js
+++ Exercice 4/exo4.js
@@ -1,28 +1,28 @@
-// Select all elements with the class 'square'
-const square = document.querySelectorAll(".square")
-// Select the container element
-const result = document.querySelector(".container")
-// Store the default background color of the container
-const defaultBgColor = getComputedStyle(result).backgroundColor;
+const squares = document.querySelectorAll(".square");
+    // Select the container element
+    const result = document.querySelector(".container");
 
-// Add a click event listener to each square
-square.forEach((square) => {
-    square.addEventListener("click", ()  => {
-        // Get the background color of the clicked square
-        const bgColor = getComputedStyle(square).backgroundColor
+    // Store the default background color of the container
+    const defaultBgColor = getComputedStyle(result).backgroundColor;
 
-        // Toggle the background color of the container
-        if (result.style.backgroundColor === bgColor) {
-            result.style.backgroundColor = defaultBgColor;
-        } else {
-            result.style.backgroundColor = bgColor;
-        }
-        
-        // Set the background color of the container to the color of the clicked square
-        result.style.backgroundColor = bgColor;
-        // Get the child [array] of the square
-        square.childNodes[0].classList.toggle("activeImg")
-        // Toggle the 'active' class on the square
-        square.classList.toggle("active")
-    })
-})
+    // Add a click event listener to each square
+    squares.forEach((square) => {
+        square.addEventListener("click", () => {
+            // Get the background color of the clicked square
+            const bgColor = getComputedStyle(square).backgroundColor;
+            
+            // Toggle the background color of the container
+            if (result.style.backgroundColor === bgColor) {
+                result.style.backgroundColor = defaultBgColor;
+            } else {
+                result.style.backgroundColor = bgColor;
+            }
+
+            // Toggle the 'activeImg' class on the img element inside the clicked square
+            const img = square.querySelector("img");
+            img.classList.toggle("activeImg");
+
+            // Toggle the 'active' class on the square
+            square.classList.toggle("active");
+        });
+    });
