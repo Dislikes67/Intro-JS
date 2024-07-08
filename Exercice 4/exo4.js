2e4d7bf7927cc0290f4f45bcaee5b8781e23d823
Mathias Rudolf <matt67creativ@hotmail.fr>
2024-07-08 16:28:03 +0200

    update class active

--- Exercice 4/exo4.js
+++ Exercice 4/exo4.js
@@ -1,9 +1,18 @@
+// Select all elements with the class 'square'
 const square = document.querySelectorAll(".square")
+// Select the container element
 const result = document.querySelector(".container")
+
+// Add a click event listener to each square
 square.forEach((square) => {
     square.addEventListener("click", ()  => {
+        // Get the background color of the clicked square
         const bgColor = getComputedStyle(square).backgroundColor
+        // Set the background color of the container to the color of the clicked square
         result.style.backgroundColor = bgColor;
-        square.childNodes[0].classList.toggle("active")
+        // Get the child [array] of the square
+        square.childNodes[0].classList.toggle("activeImg")
+        // Toggle the 'active' class on the square
+        square.classList.toggle("active")
     })
 })
