5efde45be524e3fc0f12a883f9962ae00dcd463c
Mathias Rudolf <matt67creativ@hotmail.fr>
2024-07-08 14:27:25 +0200

    minor change

--- Exercice 4/exo4.js
+++ Exercice 4/exo4.js
@@ -1,8 +1,8 @@
 const square = document.querySelectorAll(".square")
 const result = document.querySelector(".container")
 square.forEach((square) => {
-    square.addEventListener('click', ()  => {
-        const bgcolor = getComputedStyle(square).backgroundColor
+    square.addEventListener("click", ()  => {
+        const bgColor = getComputedStyle(square).backgroundColor
         result.style.backgroundColor = bgColor.backgroundColor;
         square.classList.toggle("active")
     })
